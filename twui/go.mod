061c7e025f5fad07c416f6b8cce6027413590912
Yurii Zinets <yurii.zinets@icloud.com>
2022-04-10 18:27:26 +0200

    upgrade to latest kyoto version

--- twui/go.mod
+++ twui/go.mod
@@ -2,4 +2,6 @@ module github.com/kyoto-framework/uikit/twui
 
 go 1.17
 
-require github.com/kyoto-framework/kyoto v0.1.3-0.20211129210257-b126086a72f5
+require github.com/kyoto-framework/kyoto v0.2.1-0.20220410124033-0d16c397f65f
+
+require github.com/kyoto-framework/scheduler v0.0.0-20220208110634-a2f0babca15f // indirect
--- twui/serve.go
+++ twui/serve.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/kyoto-framework/kyoto"
 	"github.com/kyoto-framework/kyoto/actions"
 	"github.com/kyoto-framework/kyoto/render"
 	"github.com/kyoto-framework/kyoto/smode"
@@ -13,8 +14,8 @@ func serve(page smode.Page) {
 	http.HandleFunc("/", render.PageHandler(
 		smode.Adapt(page),
 	))
-	http.HandleFunc("/internal/actions/", actions.Handler(func() *template.Template {
-		return template.Must(template.New("Actions").Funcs(render.FuncMap()).Parse("*.html"))
+	http.HandleFunc("/internal/actions/", actions.Handler(func(c *kyoto.Core) *template.Template {
+		return template.Must(template.New("Actions").Funcs(render.FuncMap(c)).Parse("*.html"))
 	}))
 
 	println("Listening on http://localhost:25025")
